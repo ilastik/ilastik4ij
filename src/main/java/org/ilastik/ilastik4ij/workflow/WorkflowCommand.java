ilastik/ilastik4ij

commit 7b1e6023bd591da3e68a6850dd457615f84b2b50

More workflow refactoring (#111)
* Extract subprocess running logic into Subprocess
* TempDir is now a separate class
* Extract Spinner into more general StatusBar
* Merge WorkflowCommand core logic into a single method

src/main/java/org/ilastik/ilastik4ij/workflow/WorkflowCommand.java
@@ -6,6 +6,9 @@ import net.imglib2.type.NativeType;
 import org.ilastik.ilastik4ij.hdf5.Hdf5DataSetReader;
 import org.ilastik.ilastik4ij.hdf5.Hdf5DataSetWriter;
 import org.ilastik.ilastik4ij.ui.IlastikOptions;
+import org.ilastik.ilastik4ij.util.StatusBar;
+import org.ilastik.ilastik4ij.util.Subprocess;
+import org.ilastik.ilastik4ij.util.TempDir;
 import org.scijava.ItemIO;
 import org.scijava.app.StatusService;
 import org.scijava.command.ContextCommand;
@@ -16,20 +19,12 @@ import org.scijava.options.OptionsService;
 import org.scijava.plugin.Parameter;
 import org.scijava.util.PlatformUtils;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.FileVisitResult;
+import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
-import java.util.stream.Collectors;
 
 /**
  * Base class for all commands that run ilastik in a subprocess.
@@ -45,7 +40,14 @@ public abstract class WorkflowCommand<T extends NativeType<T>> extends ContextCo
     public static final String ROLE_PROBABILITIES = "Probabilities";
     public static final String ROLE_SEGMENTATION = "Segmentation";
 
-    private static final String SPINNER_CHARS = "|/-\\";
+    @Parameter(label = "Trained ilastik project file")
+    public File projectFileName;
+
+    @Parameter(label = "Raw input image")
+    public Dataset inputImage;
+
+    @Parameter(type = ItemIO.OUTPUT)
+    public ImgPlus<T> predictions;
 
     @Parameter
     private LogService logService;
@@ -56,17 +58,18 @@ public abstract class WorkflowCommand<T extends NativeType<T>> extends ContextCo
     @Parameter
     private OptionsService optionsService;
 
-    @Parameter(label = "Trained ilastik project file")
-    public File projectFileName;
-
-    @Parameter(label = "Raw input image")
-    public Dataset inputImage;
-
-    @Parameter(type = ItemIO.OUTPUT)
-    public ImgPlus<T> predictions;
-
-    private Logger logger;
-    private ScheduledExecutorService spinnerPool;
+    @Override
+    public final void run() {
+        try (StatusBar status = new StatusBar(statusService, 300)) {
+            try (TempDir tempDir = new TempDir("ilastik4ij_")) {
+                Path inputDir = Files.createDirectory(tempDir.path.resolve("inputs"));
+                Path outputDir = Files.createDirectory(tempDir.path.resolve("outputs"));
+                runImpl(status, inputDir, outputDir);
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }
+    }
 
     /**
      * Workflow-specific command-line arguments.
@@ -77,7 +80,6 @@ public abstract class WorkflowCommand<T extends NativeType<T>> extends ContextCo
 
     /**
      * Workflow-specific input datasets.
-     * <p>
      * Keys are corresponding command-line arguments without leading dashes.
      * Values are input datasets.
      */
@@ -85,74 +87,79 @@ public abstract class WorkflowCommand<T extends NativeType<T>> extends ContextCo
         return Collections.emptyMap();
     }
 
-    @Override
-    public final void run() {
-        logger = logService.subLogger(getClass().getCanonicalName(), LogLevel.INFO);
-        spinnerPool = Executors.newScheduledThreadPool(1);
+    private void runImpl(StatusBar status, Path inputDir, Path outputDir) {
+        Logger logger = logService.subLogger(getClass().getCanonicalName(), LogLevel.INFO);
 
-        try {
-            try (TempDir temp = new TempDir("ilastik4ij_")) {
-                runWithTempDirs(
-                        Files.createDirectory(temp.dir.resolve("inputs")),
-                        Files.createDirectory(temp.dir.resolve("outputs")));
-            }
-
-        } catch (Exception e) {
-            statusService.clearStatus();
-            logger.error("ilastik run failed", e);
-            throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * Write inputs, run subprocess, read outputs.
-     */
-    private void runWithTempDirs(Path inputDir, Path outputDir) throws Exception {
-        ProcessBuilder pb = initialProcessBuilder();
+        IlastikOptions opts = optionsService.getOptions(IlastikOptions.class);
+        opts.load();
+        List<String> args = commonSubprocessArgs(opts, projectFileName);
+        Map<String, String> env = subprocessEnv(opts);
 
         Map<String, Dataset> inputs = new HashMap<>(workflowInputs());
         inputs.put("raw_data", inputImage);
-        pb.command().addAll(writeInputs(inputs, inputDir));
+
+        status.withProgress("Writing inputs", inputs.entrySet(), (entry) -> {
+            Path inputPath = inputDir.resolve(entry.getKey() + ".h5");
+            args.add(String.format("--%s=%s", entry.getKey(), inputPath));
+
+            @SuppressWarnings("unchecked")
+            ImgPlus<T> inputImg = (ImgPlus<T>) entry.getValue().getImgPlus();
+
+            logger.info(String.format("Write input '%s' starting", inputPath));
+            new Hdf5DataSetWriter<>(
+                    inputImg, inputPath.toString(), "data", 1, logger, statusService).write();
+            logger.info(String.format("Write input '%s' finished", inputPath));
+        });
 
         Path outputPath = outputDir.resolve("predictions.h5");
-        pb.command().add("--output_filename_format=" + outputPath);
+        args.add("--output_filename_format=" + outputPath);
 
-        runSubprocess(pb);
+        logger.info(String.format(
+                "Subprocess starting with arguments %s and environment %s", args, env));
+        status.withSpinner("Running " + workflowName(), () -> {
+            // ilastik prints warnings to stderr, but we don't want them to be displayed as errors.
+            // Therefore, use Logger#info for both stdout and stderr.
+            // Fatal errors should still result in a non-zero exit code.
+            int exitCode = new Subprocess(args, env, logger::info).getAsInt();
+            if (exitCode != 0) {
+                throw new RuntimeException("Subprocess failed with exit code " + exitCode);
+            }
+        });
+        logger.info("Subprocess finished");
 
-        logger.info(String.format("read from %s starting", outputPath));
-        try (Spinner ignored = new Spinner("Reading predictions")) {
-            predictions = new Hdf5DataSetReader<T>(
-                    outputPath.toString(), "exported_data", "tzyxc", logger, statusService).read();
-        }
-        logger.info("read finished");
+        status.withSpinner("Reading output", () ->
+                predictions = new Hdf5DataSetReader<T>(
+                        outputPath.toString(),
+                        "exported_data",
+                        "tzyxc",
+                        logger,
+                        statusService).read());
     }
 
-    /**
-     * Create {@link ProcessBuilder} with configured command-line arguments and environment variables,
-     * but without input and output parameters.
-     */
-    private ProcessBuilder initialProcessBuilder() {
-        IlastikOptions opts = optionsService.getOptions(IlastikOptions.class);
-        opts.load();
+    private String workflowName() {
+        List<String> words = Arrays.asList(getClass().getSimpleName().split("(?=\\p{Lu})"));
+        return String.join(" ", words.subList(0, words.size() - 1));
+    }
 
-        Path exe = opts.executableFile.toPath().toAbsolutePath();
-        if (PlatformUtils.isMac() && exe.toString().endsWith(".app")) {
-            // On macOS, we need an actual executable path, not an app bundle path.
-            exe = exe.resolve("Contents").resolve("MacOS").resolve("ilastik");
+    private static List<String> commonSubprocessArgs(IlastikOptions opts, File projectFileName) {
+        Path executable = opts.executableFile.toPath().toAbsolutePath();
+        // On macOS, if an app bundle path was provided, get an actual executable path from it.
+        if (PlatformUtils.isMac() && executable.toString().endsWith(".app")) {
+            executable = executable.resolve("Contents").resolve("MacOS").resolve("ilastik");
         }
-
-        ProcessBuilder pb = new ProcessBuilder();
-        pb.command(exe.toString(),
+        return new ArrayList<>(Arrays.asList(
+                executable.toString(),
                 "--headless",
                 "--project=" + projectFileName.getAbsolutePath(),
                 "--output_format=hdf5",
                 "--output_axis_order=tzyxc",
                 "--input_axes=tzyxc",
                 "--readonly=1",
-                "--output_internal_path=exported_data");
-        pb.command().addAll(workflowArgs());
+                "--output_internal_path=exported_data"));
+    }
 
-        Map<String, String> env = pb.environment();
+    private static Map<String, String> subprocessEnv(IlastikOptions opts) {
+        Map<String, String> env = new HashMap<>();
         if (opts.numThreads >= 0) {
             env.put("LAZYFLOW_THREADS", String.valueOf(opts.numThreads));
         }
@@ -160,123 +167,6 @@ public abstract class WorkflowCommand<T extends NativeType<T>> extends ContextCo
         env.put("LANG", "en_US.UTF-8");
         env.put("LC_ALL", "en_US.UTF-8");
         env.put("LC_CTYPE", "en_US.UTF-8");
-
-        return pb;
-    }
-
-    /**
-     * Write inputs into temporary files, return command-line arguments for these inputs.
-     */
-    private List<String> writeInputs(Map<String, Dataset> inputs, Path dir) {
-        // We need a counter, but the lambda below requires an immutable reference.
-        final int[] progress = {0};
-
-        return inputs.keySet().stream().map(name -> {
-            @SuppressWarnings("unchecked")
-            ImgPlus<T> img = (ImgPlus<T>) inputs.get(name).getImgPlus();
-            Path path = dir.resolve(name + ".h5");
-
-            logger.info(String.format("write %s to %s starting", name, path));
-            statusService.showStatus(progress[0]++, inputs.size(), "Writing inputs");
-
-            new Hdf5DataSetWriter<>(img, path.toString(), "data", 1, logger, statusService).write();
-
-            logger.info("write finished");
-            return String.format("--%s=%s", name, path);
-
-        }).collect(Collectors.toList());
-    }
-
-    /**
-     * Run ilastik subprocess and wait for it to finish.
-     */
-    private void runSubprocess(ProcessBuilder pb) throws Exception {
-        logger.info(String.format(
-                "ilastik run starting with arguments %s and environment %s",
-                pb.command(),
-                pb.environment()));
-
-        Process process = pb.start();
-        redirectInputStream(process.getInputStream(), logger::info);
-        // ilastik prints warnings to stderr, but we don't want them to be displayed as errors.
-        // Fatal errors should result in a non-zero exit code.
-        redirectInputStream(process.getErrorStream(), logger::info);
-
-        // Get "My Workflow" string from a MyWorkflowCommand class.
-        String[] words = getClass().getSimpleName().split("(?=\\p{Lu})");
-        String workflowName = String.join(" ", Arrays.copyOfRange(words, 0, words.length - 1));
-
-        try (Spinner ignored = new Spinner("Running " + workflowName)) {
-            if (process.waitFor() != 0) {
-                throw new RuntimeException("exit code " + process.exitValue());
-            }
-        }
-
-        logger.info("ilastik run finished");
-    }
-
-    /**
-     * During try-with-resources block, show message with a ticking spinner in the status bar.
-     */
-    private final class Spinner implements AutoCloseable {
-        private final String message;
-        private final ScheduledFuture<?> future;
-        private int i = 0;
-
-        public Spinner(String message) {
-            this.message = message;
-            future = spinnerPool.scheduleWithFixedDelay(this::update, 0, 300, TimeUnit.MILLISECONDS);
-        }
-
-        @Override
-        public void close() {
-            future.cancel(true);
-        }
-
-        private void update() {
-            statusService.showStatus(message + " " + SPINNER_CHARS.charAt(i));
-            i = (i + 1) % SPINNER_CHARS.length();
-        }
-    }
-
-    /**
-     * Start {@link Thread} that reads UTF-8 lines from source and sends them to destination.
-     */
-    private static void redirectInputStream(InputStream src, Consumer<String> dst) {
-        new Thread(() -> {
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(src, StandardCharsets.UTF_8))) {
-                reader.lines().forEachOrdered(dst);
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
-            }
-        }).start();
-    }
-
-    /**
-     * Create temporary directory that exists within a try-with-resources block.
-     */
-    private static final class TempDir implements AutoCloseable {
-        public final Path dir;
-
-        public TempDir(String prefix) throws IOException {
-            dir = Files.createTempDirectory(prefix);
-        }
-
-        @Override
-        public void close() throws IOException {
-            Files.walkFileTree(dir, new SimpleFileVisitor<Path>() {
-                @Override
-                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                    Files.delete(file);
-                    return FileVisitResult.CONTINUE;
-                }
-
-                @Override
-                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-                    Files.delete(dir);
-                    return FileVisitResult.CONTINUE;
-                }
-            });
-        }
+        return env;
     }
 }

src/main/java/org/ilastik/ilastik4ij/util/StatusBar.java
@@ -1,0 +1,74 @@
+package org.ilastik.ilastik4ij.util;
+
+import org.scijava.app.StatusService;
+
+import java.util.Collection;
+import java.util.Objects;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+
+/**
+ * Update status bar while functions are running or collections are iterated.
+ */
+public final class StatusBar implements AutoCloseable {
+    private static final String SPINNER_CHARS = "|/-\\";
+
+    private final ScheduledExecutorService pool;
+    private final StatusService status;
+    private final int period;
+
+    public StatusBar(StatusService statusService, int updatePeriodMillis) {
+        pool = Executors.newScheduledThreadPool(1);
+        status = Objects.requireNonNull(statusService);
+        period = updatePeriodMillis;
+        if (updatePeriodMillis <= 0) {
+            throw new IllegalArgumentException("update period should be positive");
+        }
+    }
+
+    @Override
+    public void close() {
+        pool.shutdown();
+        status.clearStatus();
+    }
+
+    /**
+     * Periodically update status bar from another thread by showing message with textual spinner.
+     */
+    public void withSpinner(String message, Runnable func) {
+        Objects.requireNonNull(message);
+        Objects.requireNonNull(func);
+
+        final int[] index = {0};
+        Runnable update = () -> {
+            status.showStatus(message + " " + SPINNER_CHARS.charAt(index[0]));
+            index[0] = (index[0] + 1) % SPINNER_CHARS.length();
+        };
+
+        ScheduledFuture<?> sf = pool.scheduleAtFixedRate(update, 0, period, TimeUnit.MILLISECONDS);
+        try {
+            func.run();
+        } finally {
+            sf.cancel(true);
+        }
+    }
+
+    /**
+     * Update status bar each time a new collection item is processed.
+     */
+    public <E> void withProgress(String message, Collection<E> items, Consumer<E> func) {
+        Objects.requireNonNull(message);
+        Objects.requireNonNull(items);
+        Objects.requireNonNull(func);
+
+        int progress = 0;
+        status.showStatus(progress++, items.size(), message);
+        for (E item : items) {
+            func.accept(item);
+            status.showStatus(progress++, items.size(), message);
+        }
+    }
+}

src/main/java/org/ilastik/ilastik4ij/util/Subprocess.java
@@ -1,0 +1,82 @@
+package org.ilastik.ilastik4ij.util;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
+import java.util.function.IntSupplier;
+
+/**
+ * Wrapper around {@link ProcessBuilder}.
+ * Run subprocess with the specified arguments and environment,
+ * optionally capturing output and error streams.
+ */
+public final class Subprocess implements IntSupplier {
+    private final List<String> args;
+    private final Map<String, String> env;
+    private final Consumer<String> stdout;
+    private final Consumer<String> stderr;
+
+    public Subprocess(String arg) {
+        this(Collections.singletonList(arg));
+    }
+
+    public Subprocess(List<String> args) {
+        this(args, Collections.emptyMap());
+    }
+
+    public Subprocess(List<String> args, Map<String, String> env) {
+        this(args, env, null);
+    }
+
+    public Subprocess(List<String> args, Map<String, String> env, Consumer<String> sink) {
+        this(args, env, sink, sink);
+    }
+
+    public Subprocess(
+            List<String> args,
+            Map<String, String> env,
+            Consumer<String> stdout,
+            Consumer<String> stderr) {
+        this.args = Objects.requireNonNull(args);
+        this.env = Objects.requireNonNull(env);
+        this.stdout = stdout;
+        this.stderr = stderr;
+    }
+
+    @Override
+    public int getAsInt() {
+        ExecutorService pool = Executors.newFixedThreadPool(2);
+        ProcessBuilder pb = new ProcessBuilder(args);
+        pb.environment().putAll(env);
+        try {
+            Process process = pb.start();
+            redirect(pool, process.getInputStream(), stdout);
+            redirect(pool, process.getErrorStream(), stderr);
+            return process.waitFor();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            pool.shutdown();
+        }
+    }
+
+    private static void redirect(ExecutorService pool, InputStream src, Consumer<String> dst) {
+        if (dst == null) {
+            return;
+        }
+        pool.submit(() -> {
+            try (BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(src, StandardCharsets.UTF_8))) {
+                reader.lines().forEachOrdered(dst);
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        });
+    }
+}

src/main/java/org/ilastik/ilastik4ij/util/TempDir.java
@@ -1,0 +1,36 @@
+package org.ilastik.ilastik4ij.util;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Objects;
+
+/**
+ * Create a temporary directory {@link #path}, and recursively delete it on {@link #close()}.
+ */
+public final class TempDir implements AutoCloseable {
+    private static final FileVisitor<Path> RECURSIVE_DELETE = new SimpleFileVisitor<Path>() {
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            Files.delete(file);
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+            Files.delete(dir);
+            return FileVisitResult.CONTINUE;
+        }
+    };
+
+    public final Path path;
+
+    public TempDir(String prefix) throws IOException {
+        path = Files.createTempDirectory(Objects.requireNonNull(prefix));
+    }
+
+    @Override
+    public void close() throws IOException {
+        Files.walkFileTree(path, RECURSIVE_DELETE);
+    }
+}

commit f89e23e04fb5dc47fd2882c591f002933805b8c6

Bugfix: workflow args were not passed to subprocess
This fixes bug introduced in https://github.com/ilastik/ilastik4ij/pull/111.

src/main/java/org/ilastik/ilastik4ij/workflow/WorkflowCommand.java
@@ -92,8 +92,9 @@ public abstract class WorkflowCommand<T extends NativeType<T>> extends ContextCo
 
         IlastikOptions opts = optionsService.getOptions(IlastikOptions.class);
         opts.load();
+
         List<String> args = commonSubprocessArgs(opts, projectFileName);
-        Map<String, String> env = subprocessEnv(opts);
+        args.addAll(workflowArgs());
 
         Map<String, Dataset> inputs = new HashMap<>(workflowInputs());
         inputs.put("raw_data", inputImage);
@@ -114,6 +115,7 @@ public abstract class WorkflowCommand<T extends NativeType<T>> extends ContextCo
         Path outputPath = outputDir.resolve("predictions.h5");
         args.add("--output_filename_format=" + outputPath);
 
+        Map<String, String> env = subprocessEnv(opts);
         logger.info(String.format(
                 "Subprocess starting with arguments %s and environment %s", args, env));
         status.withSpinner("Running " + workflowName(), () -> {