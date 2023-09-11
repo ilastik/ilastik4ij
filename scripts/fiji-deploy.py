9d79f72fc8c47c05b79ed0136bcb2df5841e3b69
Emil Melnikov <emilmelnikov@gmail.com>
2023-09-11 11:33:08 +0200

    Improve fiji-deploy.py
    * Ensure that Maven is installed
    * Remove old jar after successful build
    * Don't show stack trace on KeyboardInterrupt

--- scripts/fiji-deploy.py
+++ scripts/fiji-deploy.py
@@ -5,6 +5,7 @@
 import argparse
 import shutil
 import subprocess
+import sys
 from pathlib import Path
 
 
@@ -13,25 +14,32 @@ def main():
     ap.add_argument("fiji", type=Path, help="Path to Fiji directory")
     args = ap.parse_args()
 
-    for subdir in ("plugins", "jars"):
-        for old_jar in (args.fiji / subdir).glob("ilastik4ij*.jar"):
-            print(f"removing old jar {old_jar}")
-            old_jar.unlink()
+    if not shutil.which("mvn"):
+        sys.exit("error: maven not found in system path")
 
     print("cleaning project")
     subprocess.run(["mvn", "clean"], check=True)
 
-    print(f"building project")
+    print("building project")
     subprocess.run(["mvn", "package", "-DskipTests=true"], check=True)
 
     jars = list(Path("target").glob("ilastik4ij-*-SNAPSHOT.jar"))
     if len(jars) != 1:
-        raise RuntimeError(f"Expected exactly one jar, found {jars}")
+        sys.exit(f"error: expected exactly one jar, found {jars}")
     jar = jars[0]
+
+    for subdir in ("plugins", "jars"):
+        for old_jar in (args.fiji / subdir).glob("ilastik4ij*.jar"):
+            print(f"removing {old_jar}")
+            old_jar.unlink()
+
     plugins_dir = args.fiji / "plugins"
     print(f"copying {jar} to {plugins_dir}")
     shutil.copy(jar, plugins_dir)
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
