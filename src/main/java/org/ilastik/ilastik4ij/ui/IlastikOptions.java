4771d5b3804d71bfc5075f1eeaf9f1d715094de6
Emil Melnikov <emilmelnikov@gmail.com>
2023-09-11 13:31:18 +0200

    Update options look&feel
    * Bump ilastik version in examples
    * Move example paths into separate MESSAGE parameter

--- src/main/java/org/ilastik/ilastik4ij/ui/IlastikOptions.java
+++ src/main/java/org/ilastik/ilastik4ij/ui/IlastikOptions.java
@@ -1,6 +1,5 @@
 package org.ilastik.ilastik4ij.ui;
 
-import org.scijava.module.MutableModuleItem;
 import org.scijava.options.OptionsPlugin;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
@@ -9,33 +8,36 @@ import org.scijava.widget.FileWidget;
 
 import java.io.File;
 
+import static org.scijava.ItemVisibility.MESSAGE;
+
 @Plugin(type = OptionsPlugin.class, menuPath = "Plugins>ilastik>Configure ilastik executable location")
 public final class IlastikOptions extends OptionsPlugin {
     @Parameter(label = "Path to ilastik executable", style = FileWidget.OPEN_STYLE)
-    public File executableFile = new File("/opt/ilastik/run_ilastik.sh");
+    public File executableFile;
 
-    @Parameter(label = "Number of Threads ilastik is allowed to use.\nNegative numbers means no restriction")
+    @Parameter(label = "Number of Threads ilastik is allowed to use. Negative numbers means no restriction")
     public int numThreads = -1;
 
     @Parameter(min = "256", label = "Maximum amount of RAM (in MB) that ilastik is allowed to use.")
     public int maxRamMb = 4096;
 
+    @Parameter(visibility = MESSAGE)
+    private String examplePath = "";
+
     @Override
     public void initialize() {
-        MutableModuleItem<File> item = getInfo().getMutableInput("executableFile", File.class);
         String path = "";
-
         if (PlatformUtils.isWindows()) {
-            path = "C:\\\\Program Files\\ilastik-1.3.3\\ilastik.exe";
+            path = "C:\\\\Program Files\\ilastik-1.4.0\\ilastik.exe";
         } else if (PlatformUtils.isLinux()) {
-            path = "/opt/ilastik-1.3.3-Linux/run_ilastik.sh";
+            path = "/opt/ilastik-1.4.0-Linux/run_ilastik.sh";
         } else if (PlatformUtils.isMac()) {
-            path = "/Applications/ilastik-1.3.3-OSX.app/Contents/MacOS/ilastik";
-            item.setWidgetStyle(FileWidget.DIRECTORY_STYLE);
+            path = "/Applications/ilastik-1.4.0-OSX.app/Contents/MacOS/ilastik";
         }
 
         if (!path.isEmpty()) {
-            item.setLabel(item.getLabel() + ", e.g. " + path);
+            getInfo().getMutableInput("examplePath", String.class).setLabel("Example path");
+            examplePath = "<pre>" + path + "</pre>";
         }
     }
 }
